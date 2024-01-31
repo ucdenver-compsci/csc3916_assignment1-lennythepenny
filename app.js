9639560badc39d160253f4a818057f4c742651c3
lennythepenny <lena.sarieva@gmail.com>
2024-01-30 20:54:02 -0700

    API Changes to Express

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
-var server = require("http").createServer();
-
-server.on("request", (request, response) => {
-    var body = [];
-    request.on("data", chunk => {
-        body.push(chunk);
-    });
-    request
-        .on("end", () => {
-            let bodyString = body.concat().toString();
-            console.log(bodyString);
-            response.end(bodyString);
-        })
-        .on("error", () => {
-            response.statusCode = 400;
-            response.end();
-        });
-    response.on("error", err => {
-        console.error(err);
-    });
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express()
+app.use(bodyParser.text({
+    type: function(req){
+        return 'text';
+    }
+}));
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    res = res.status(200);
+    var contentType = req.get('Content-Type');
+    if(contentType){
+        console.log("Content type:"+ contentType);
+        res = res.type(contentType)
+    }
+    res.send(req.body)
 });
-server.listen(process.env.PORT || 8008, () => {
-    console.log("Server listening at 8008");
-});
-
-module.exports = server; // for testing
+app.listen(process.env.PORT || 8080);
+module.exports = app; // for testing
 
 //curl -d "echo" -H "Content-Type: text" -X POST http://localhost:8008
